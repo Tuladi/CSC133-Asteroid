be2d4d635ee6a2b4ea8053853e80a5056cc64ea0
Tuladi <tuladhar.aditya@gmail.com>
2019-06-27 18:59:40 -0700

    added graphics package and asteroid shape class

---
+++ src/com/mycompany/a2/graphics/Asteroid.java
@@ -0,0 +1,16 @@
+package com.mycompany.a2.graphics;
+
+public class Asteroid {
+	int iShapeX;
+	int iShapeY;
+	int width, height;
+	int color;
+	
+	public Asteroid(int iShapeX, int iShapeY, int width, int height, int color) {
+		this.iShapeX = iShapeX;
+		this.iShapeY = iShapeY;
+		this.color = color;
+		this.width = width;
+		this.height = height;
+	}
+}
